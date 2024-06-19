857cc72085d5c6a4be2e46d14521eb5dd871974b
Robert Chapman <chapmanrobert12@gmail.com>
2024-06-18 23:01:47 -0500

    Created basic ui layout for the app. Uses dropzone hook for the dropzone

--- src/app/components/imageUploader.tsx
+++ src/app/components/imageUploader.tsx
@@ -1,3 +1,18 @@
+'use client'
+
+import { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+import styles from './imageUploader.module.css';
+
 export default function ImageUploader() {
-  return <>Image Upload Component</>;
+  const onDrop = useCallback((acceptedFiles: unknown) => {
+    console.log(acceptedFiles);
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return <div {...getRootProps()} className={styles.dropzone}>
+    <input {...getInputProps()} />
+    {isDragActive ? <p>Drop the files here</p> : <p>Drag {'n'} drop files here, or click to select files</p>}
+  </div>;
 }
